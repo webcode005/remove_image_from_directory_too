2b9e0358774951b42e35e1e3b89459a9d153c8dc
Basant Mallick <87462488+webcode005@users.noreply.github.com>
2022-04-01 18:08:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include'db.php';
 
 
 
-public function removeFile($filename) 
+ function removeFile($filename) 
   {
   $path = "uploads/";
     $files = glob($path . $filename);
@@ -27,7 +27,7 @@ public function removeFile($filename)
   $file_name=$_GET['file'];
    $id=$_GET['id'];
   
-  $conn->query("DELETE FROM tbl_name file_name='' WHERE id='$id';
+  $conn->query("DELETE FROM tbl_name file_name='' WHERE id='$id'");
   removeFile($file_name);
   header("Location:index.php");
 
